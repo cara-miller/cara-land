9b21a1c968b1e017c4e1e15bc24fbfeffa3b6a45
cara-miller <ca.miller92@gmail.com>
2019-06-24 16:24:01 -0400

    config adjustments

---
+++ src/components/layout.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, Link, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+
+export default ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      {children}
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+const divStyle = { textAlign: "center" }
+export default () => (
+  <div>
+    <h1 style={divStyle}>Cara M. Miller</h1>
+    <Layout>
+      <h2>About</h2>
+      <p>bla bla</p>
+    </Layout>
+  </div>
+)

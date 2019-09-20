5997ba4c1b801ecc3433a7eb77f0ed257d4d4ac6
cara-miller <ca.miller92@gmail.com>
2019-09-20 14:49:21 -0400

    add sass & react-icons to configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `The real Title`,
+    title: `The real Title`
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
     `gatsby-transformer-remark`,
-    `gatsby-plugin-emotion`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
+  ]
 }

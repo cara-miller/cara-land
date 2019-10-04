d0dc1bbab858c3493746812c2cf754d8bef21386
cara-miller <ca.miller92@gmail.com>
2019-10-04 14:48:07 -0400

    style adjustments for articles longer than vh

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,13 +3,15 @@ import Article from "../components/Article"
 
 export default function Hero() {
   return (
-    <Article id="hero" articleClass="article__no-border">
-      <h1 className="hero-header">Hi, I&apos;m Cara Margot.</h1>
-      <h2 className="hero-subheader">
-        Web development,
-        <br />
-        UX research, design systems, <br />& general front end magic
-      </h2>
-    </Article>
+    <article id="hero" className="article">
+      <div className="article-wrapper article__no-border">
+        <h1 className="hero-header">Hi, I&apos;m Cara Margot.</h1>
+        <h2 className="hero-subheader">
+          Web development,
+          <br />
+          UX research, design systems, <br />& general front end magic
+        </h2>
+      </div>
+    </article>
   )
 }

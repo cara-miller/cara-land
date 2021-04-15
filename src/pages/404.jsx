bafa9c4e5ae65aee334e14b01a25208c83679cf9
Cara <ca.miller92@gmail.com>
2021-04-15 16:08:52 -0400

    add 404 page placeholder

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../layouts/Layout"
+
+const YouLostPage = () => (
+    <Layout>
+        <p>oopsies there's nothing here</p>
+    </Layout>
+  )
+  export default YouLostPage;
